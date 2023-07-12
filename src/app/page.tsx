81bb3d7b41c02afc3f6e4ff6d2919601798ff253
tzhangchi <c@affine.pro>
2023-07-12 19:34:52 +0800

    feat: test openapi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
-
+import PostEditorPage from "./pages/check";
 import dynamic from "next/dynamic";
 const Editor = dynamic(() => import("@/components/editor/editor"), {
   ssr: false,
 });
 
 export default function Home() {
-  return <Editor />;
+  return (
+    <div>
+      <PostEditorPage />
+      {/* <Editor /> */}
+    </div>
+  );
 }
