ce8a285d7f3941f3f04ac5586a65c34dd362a21b
tzhangchi <c@affine.pro>
2023-07-12 21:45:25 +0800

    feat: show all ai page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
-// import PostEditorPage from "./pages/check";
-
+import PostEditorPage from "./pages/check";
 import Chat from "./pages/chat";
 import dynamic from "next/dynamic";
 const Editor = dynamic(() => import("@/components/editor/editor"), {
@@ -10,9 +9,9 @@ const Editor = dynamic(() => import("@/components/editor/editor"), {
 export default function Home() {
   return (
     <div>
-      {/* <PostEditorPage /> */}
+      <PostEditorPage />
       <Chat />
-      {/* <Editor /> */}
+      <Editor />
     </div>
   );
 }
