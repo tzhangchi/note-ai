acfca9710fd6d1d6b685de8cd4e084e6774fc9b8
tzhangchi <c@affine.pro>
2023-07-12 19:44:05 +0800

    feat: add chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
-import PostEditorPage from "./pages/check";
+// import PostEditorPage from "./pages/check";
+
+import Chat from "./pages/chat";
 import dynamic from "next/dynamic";
 const Editor = dynamic(() => import("@/components/editor/editor"), {
   ssr: false,
@@ -8,7 +10,8 @@ const Editor = dynamic(() => import("@/components/editor/editor"), {
 export default function Home() {
   return (
     <div>
-      <PostEditorPage />
+      {/* <PostEditorPage /> */}
+      <Chat />
       {/* <Editor /> */}
     </div>
   );
