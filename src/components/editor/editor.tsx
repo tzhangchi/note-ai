e4914ea76267b5b45d67507338e112d9a373459e
tzhangchi <c@affine.pro>
2023-07-13 09:08:47 +0800

    feat: update github link

--- src/components/editor/editor.tsx
+++ src/components/editor/editor.tsx
@@ -232,7 +232,7 @@ const Editor: React.FC<IEditorProps> = (props) => {
   return (
     <div className="flex min-h-screen flex-col items-center sm:px-5 sm:pt-[calc(15vh)]">
       <a
-        href="https://github.com/toeverything/blocksuite"
+        href="https://github.com/tzhangchi/note-ai"
         target="_blank"
         className="print:hidden fixed bottom-5 left-5 z-10 max-h-fit rounded-lg p-2 transition-colors duration-200 hover:bg-stone-100 sm:bottom-auto sm:top-5"
       >
